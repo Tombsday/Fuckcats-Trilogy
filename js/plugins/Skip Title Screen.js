17ef4b84ede4314ca60214253a58ca199e85d761
Tombsday <34579108+Tombsday@users.noreply.github.com>
2018-01-15 00:52:03 -0600

    Big Commit

---
+++ js/plugins/Skip Title Screen.js
@@ -0,0 +1,23 @@
+/*:
+ * @plugindesc Skips the title screen.
+ * @version 1.0
+ * @author Yami
+ */
+
+(function() {
+
+    Scene_Boot.prototype.start = function() {
+        Scene_Base.prototype.start.call(this);
+        SoundManager.preloadImportantSounds();
+        if (DataManager.isBattleTest()) {
+            DataManager.setupBattleTest();
+            SceneManager.goto(Scene_Battle);
+        } else {
+            this.checkPlayerLocation();
+            DataManager.setupNewGame();
+            SceneManager.goto(Scene_Map);
+        }
+        this.updateDocumentTitle();
+    };
+
+})();
